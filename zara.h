2955cfb6565d8d461028b211a8b22d8c12597d78
Анастасія Варяниця <anastasia.00nastya00@gmail.com>
2026-02-26 21:39:10 +0200

    edits

--- zara.h
+++ zara.h
@@ -5,7 +5,7 @@ class zara: public shop{
     public:
     zara(): shop(20){};
     void print(){
-        cout<<"bill\n";
+        cout<<"    bill\n";
         cout<<"------------------\n";
         cout<<"Zara\n";
         cout<<"discount: "<<discount<<"%\n";
