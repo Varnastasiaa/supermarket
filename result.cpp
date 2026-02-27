5350e7394b2b245befb9cce28874b4c9f2a89536
Анастасія Варяниця <anastasia.00nastya00@gmail.com>
2026-02-27 09:20:26 +0200

    new file

---
+++ result.cpp
@@ -0,0 +1,29 @@
+#include "zara.h"
+#include "bershkaN.h"
+int main(){
+    int choice;
+
+    cout<<"choose your shop\n";
+    cout<<"Zara=1\n";
+    cout<<"------------------\n";
+    cout<<"bershka=2\n";
+    cout<<"------------------\n";
+    cout<<"youre choi.      ce: ";
+    cin>>choice;
+    cout<<endl;
+
+    shop* shop=nullptr;
+
+    if(choice==1){
+        shop= new zara();
+    }
+    else if(choice==2){
+        shop= new bershka();
+    }
+    else{
+        cout<<"invalid choice\n";
+    }
+    shop->print();
+    delete shop;
+    return 0;
+}
